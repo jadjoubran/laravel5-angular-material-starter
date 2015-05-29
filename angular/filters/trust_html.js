2a8096035db72b86a1a05bc85e509d98a7e86aea
Infacq <muhaimincs@itronics.com.my>
2015-05-29 16:39:15 +0800

    remove angular

--- angular/filters/trust_html.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	"use strict";
-
-	angular.module('app.filters').filter( 'trustHtml', function( $sce ){
-		return function( html ){
-			return $sce.trustAsHtml(html);
-		};
-	});
-})();
