2b968c00f9d093fd84c1d2048b61cc217b3bb6f6
Jad Joubran <jad@jadjoubran.com>
2015-09-05 15:01:53 +0300

    AngularController unit tests

---
+++ tests/LaravelRoutesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class LaravelRoutesTest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testLandingResponseCode()
+    {
+        $response = $this->call('GET', '/');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function testServeAngular()
+    {
+        $this->visit('/')
+             ->see('ng-app');
+    }
+
+    public function testUnsupportedBrowserPage()
+    {
+        $this->visit('/unsupported-browser')
+             ->see('update your browser')
+             ->see('Internet Explorer');
+    }
+}
