532a18dd868142c3da3d51f0b5eb355c07046ca0
Jad Joubran <joubran.jad@gmail.com>
2015-09-12 09:13:18 -0400

    Applied fixes from StyleCI

--- tests/AngularGeneratorCommandsTest.php
+++ tests/AngularGeneratorCommandsTest.php
@@ -4,17 +4,16 @@ class AngularGeneratorCommandsTest extends TestCase
 {
     public function testNgGeneratorsExist()
     {
-       $exitCode = Artisan::call('list');
-       $output = Artisan::output();
+        $exitCode = Artisan::call('list');
+        $output = Artisan::output();
 
-       $this->assertEquals($exitCode, 0);
+        $this->assertEquals($exitCode, 0);
 
-       $this->assertContains('ng:feature', $output);
-       $this->assertContains('ng:dialog', $output);
-       $this->assertContains('ng:directive', $output);
-       $this->assertContains('ng:service', $output);
-       $this->assertContains('ng:filter', $output);
-       $this->assertContains('ng:config', $output);
-   }
-
+        $this->assertContains('ng:feature', $output);
+        $this->assertContains('ng:dialog', $output);
+        $this->assertContains('ng:directive', $output);
+        $this->assertContains('ng:service', $output);
+        $this->assertContains('ng:filter', $output);
+        $this->assertContains('ng:config', $output);
+    }
 }
