0b901c533b7babebe9fb7bf8615831a800d37648
Jad Joubran <joubran.jad@gmail.com>
2015-09-16 16:58:17 -0400

    Applied fixes from StyleCI

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,23 +15,20 @@ Route::get('/', 'AngularController@serveApp');
 
 Route::get('/unsupported-browser', 'AngularController@unsupported');
 
-
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', function($api){
+$api->version('v1', function ($api) {
     /*
      * used for Json Web Token Authentication - https://scotch.io/tutorials/token-based-authentication-for-angularjs-and-laravel-apps
      * Make sure to re-enable CSRF middleware if you're disabling JWT
      */
     $api->controller('authenticate', 'App\Http\Controllers\AuthenticateController');
 
-
     $api->get('test', 'App\Http\Controllers\WelcomeController@getSample');
 });
 
-
 //protected with JWT
-$api->version('v1', ['middleware' => 'api.auth'], function($api){
+$api->version('v1', ['middleware' => 'api.auth'], function ($api) {
 
     $api->post('test/sample', 'App\Http\Controllers\WelcomeController@sample');
 
