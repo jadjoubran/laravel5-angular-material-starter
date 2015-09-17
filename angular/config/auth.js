0016ece5b1486d2743d0c367b82923edf4c58423
Jad Joubran <jad@jadjoubran.com>
2015-09-17 17:43:22 +0300

    fixed jwt route to conform with dingo/api #43

--- angular/config/auth.js
+++ angular/config/auth.js
@@ -4,7 +4,7 @@
     angular.module('app.config').config(function ($authProvider){
         // Satellizer configuration that specifies which API
         // route the JWT should be retrieved from
-        $authProvider.loginUrl = '/api/1/authenticate/auth';
+        $authProvider.loginUrl = '/api/authenticate/auth';
     });
 
 })();
