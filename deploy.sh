4e6d8cf9561ee736a6723765aac82da09e06a337
Jad Joubran <jad@jadjoubran.com>
2015-09-19 13:04:43 +0300

    force run migrations in production - closes #56

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 #ssh to your server here
 git pull
 composer install --no-dev
-php artisan migrate
+php artisan migrate --force
 php artisan route:cache
 php artisan config:cache
 php artisan optimize
