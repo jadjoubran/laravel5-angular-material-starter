ba38631c0686902526ef4195d67488110eed13d5
Jad Joubran <joubran.jad@gmail.com>
2015-11-12 18:07:53 +0200

    removed deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#ssh to your server here
-git pull
-composer install --no-dev
-php artisan migrate --force
-php artisan route:cache
-php artisan config:cache
-php artisan optimize
