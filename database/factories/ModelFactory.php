a6499c6440de64c2b633151aa8da4ef4b6e393a0
Jad Joubran <joubran.jad@gmail.com>
2015-11-25 15:33:19 +0200

    Increase tests speed - closes #109

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,7 +15,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
+        'password' => str_random(36),
         'remember_token' => str_random(10),
     ];
 });
