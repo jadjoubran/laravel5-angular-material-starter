7b5b5a4fbeae48d97abdb352b4722e24efc9c691
Jad Joubran <joubran.jad@gmail.com>
2015-11-29 23:56:08 +0200

    Updated Angular Generators to 1.1 - supports custom file names

--- config/generators.php
+++ config/generators.php
@@ -2,38 +2,56 @@
 
 return [
 
-    /*
-     * Angular source directory name
-     */
-    'source'    => 'angular',
-
-    /*
-     * Angular config directory name
-     */
-    'config'    => 'config',
-
-    /*
-     * Angular dialog directory name
-     */
-    'dialogs'    => 'dialogs',
-
-    /*
-     * Angular directive directory name
-     */
-    'directives' => 'directives',
-
-    /*
-     * Angular feature directory name
-     */
-    'features'   => 'app',
-
-    /*
-     * Angular filter directory name
-     */
-    'filters'    => 'filters',
-
-    /*
-     * Angular serivce directory name
-     */
-    'services'   => 'services',
+      'source' => [
+            /*
+             * Angular source directory name
+             */
+            'main'            => 'angular',
+            /*
+             * Angular config directory name
+             */
+            'config'          => 'config',
+            /*
+             * Angular dialog directory name
+             */
+            'dialogs'         => 'dialogs',
+            /*
+             * Angular directive directory name
+             */
+            'directives'      => 'directives',
+            /*
+             * Angular feature directory name
+             */
+            'features'        => 'app',
+            /*
+             * Angular filter directory name
+             */
+            'filters'         => 'filters',
+            /*
+             * Angular serivce directory name
+             */
+            'services'        => 'services',
+      ],
+      'prefixFileNames' => [
+            /*
+             * Angular controller 
+             */
+            'controller'      => '.controller.js',
+            /*
+             * Angular directive 
+             */
+            'directive'       => '.directive.js',
+            /*
+             * Angular dialog 
+             */
+            'dialog'          => '.dialog.js',
+            /*
+             * Angular definition 
+             */
+            'definition'      => '.definition.js',
+            /*
+             * Angular service
+             */
+            'service'         => '.service.js',
+      ],
 ];
