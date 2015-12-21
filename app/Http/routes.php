90d8a8907706c96597dbd3264fd4ea81fbc70aa1
Jad Joubran <joubran.jad@gmail.com>
2015-12-21 19:20:30 +0200

    fixed API version in routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,14 +4,14 @@ Route::get('/', 'AngularController@serveApp');
 
 Route::get('/unsupported-browser', 'AngularController@unsupported');
 
-$api->version('v1', function ($api) {
+$api->group([], function ($api) {
 
     $api->post('users/login', 'LoginController@login');
 
 });
 
 //protected routes with JWT (must be logged in to access any of these routes)
-$api->version('v1', ['middleware' => 'api.auth'], function ($api) {
+$api->group(['middleware' => 'api.auth'], function ($api) {
 
     $api->get('sample/protected', 'LoginController@protectedData');
 
