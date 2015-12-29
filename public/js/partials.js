f41ad9f4535b44a6e37a8e56ae257ffc8fabab04
Jad Joubran <joubran.jad@gmail.com>
2015-12-29 23:42:08 +0200

    Design review for demo

--- public/js/partials.js
+++ public/js/partials.js
@@ -5,40 +5,12 @@ try {
   module = angular.module('partialsModule', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('./views/app/header/header.html',
-    '<md-content class="Page-Container DemoHeader" ng-controller="HeaderController as vm">\n' +
-    '	<div layout="row">\n' +
-    '		<div flex="80" flex-offset="10">\n' +
-    '			<div class="DemoHeader-container">\n' +
-    '				<div layout="row" layout-align="space-between">\n' +
-    '					<img src="img/laravel-angular-logo.svg" class="DemoHeader-logo"/>\n' +
-    '					<div>\n' +
-    '						<a class="DemoHeader-link" href="https://laravel-angular.readme.io" target="_blank">Docs</a>\n' +
-    '						<a class="DemoHeader-link" href="">Screencasts</a>\n' +
-    '						<a class="DemoHeader-link" href="https://github.com/jadjoubran/laravel5-angular-material-starter" target="_blank">Github</a>\n' +
-    '					</div>\n' +
-    '				</div>\n' +
-    '			</div>\n' +
-    '		</div>\n' +
-    '	</div>\n' +
-    '</md-content>\n' +
-    '');
-}]);
-})();
-
-(function(module) {
-try {
-  module = angular.module('partialsModule');
-} catch (e) {
-  module = angular.module('partialsModule', []);
-}
-module.run(['$templateCache', function($templateCache) {
   $templateCache.put('./views/app/footer/footer.html',
     '<md-content class="Page-Container Footer" ng-controller="FooterController as vm" layout-align="center center">\n' +
-    '	<md-icon md-svg-src="/img/icons/logo.svg"></md-icon>\n' +
+    '	<md-icon md-svg-src="/img/icons/logo-grey.svg"></md-icon>\n' +
     '	<br/>\n' +
     '	<br/>\n' +
-    '	<div>\n' +
+    '	<div class="Footer-text">\n' +
     '		An open source project by <a href="https://github.com/jadjoubran" class="Footer-link" target="_blank"><strong>Jad Joubran</strong></a>.\n' +
     '		Design by <a href="https://www.linkedin.com/in/nicolesaidy" class="Footer-link" target="_blank"><strong>Nicole Saidy</strong></a>\n' +
     '	</div>\n' +
@@ -58,11 +30,39 @@ try {
   module = angular.module('partialsModule', []);
 }
 module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('./views/app/header/header.html',
+    '<md-content class="Page-Container DemoHeader" ng-controller="HeaderController as vm">\n' +
+    '	<div layout="row">\n' +
+    '		<div flex="80" flex-offset="10">\n' +
+    '			<div class="DemoHeader-container">\n' +
+    '				<div layout="row" layout-align="space-between">\n' +
+    '					<img src="img/icons/logo.svg" class="DemoHeader-logo"/>\n' +
+    '					<div>\n' +
+    '						<a class="DemoHeader-link md-subhead" href="https://laravel-angular.readme.io" target="_blank">Docs</a>\n' +
+    '						<a class="DemoHeader-link md-subhead" href="">Screencasts</a>\n' +
+    '						<a class="DemoHeader-link md-subhead" href="https://github.com/jadjoubran/laravel5-angular-material-starter" target="_blank">Github</a>\n' +
+    '					</div>\n' +
+    '				</div>\n' +
+    '			</div>\n' +
+    '		</div>\n' +
+    '	</div>\n' +
+    '</md-content>\n' +
+    '');
+}]);
+})();
+
+(function(module) {
+try {
+  module = angular.module('partialsModule');
+} catch (e) {
+  module = angular.module('partialsModule', []);
+}
+module.run(['$templateCache', function($templateCache) {
   $templateCache.put('./views/app/landing/landing.html',
     '<div class="Page-Container Landing" ng-controller="LandingController as vm">\n' +
     '	<div layout="column" class="Landing-cover" layout-align="center center">\n' +
     '		<div class="md-body-1 Landing-subtitle">A starter project for Laravel & Angular using Angular Material.</div>\n' +
-    '		<h1 class="md-display-1 Landing-heading">Laravel Angular Material Starter</h1>\n' +
+    '		<h1 class="md-display-3 Landing-heading">Laravel Angular Material Starter</h1>\n' +
     '		<md-button class="md-raised md-large Landing-getStarted" href="https://laravel-angular.readme.io/docs/install" target="_blank">Get Started</md-button>\n' +
     '	</div>\n' +
     '\n' +
@@ -70,7 +70,7 @@ module.run(['$templateCache', function($templateCache) {
     '		<div class="Landing-ampersand">&amp;</div>\n' +
     '		<div layout="row">\n' +
     '			<div flex="50" class="Landing-laravel" layout-align="center center">\n' +
-    '				<h2 class="md-display-1 Landing-laravelAngular-title">Laravel</h2>\n' +
+    '				<h2 class="md-display-2 Landing-laravelAngular-title">Laravel</h2>\n' +
     '				<div class="md-body-1 Landing-laravelAngular-subtitle">{{vm.laravel_description}}</div>\n' +
     '				<br/>\n' +
     '				<div class="DemoCode">\n' +
@@ -87,7 +87,7 @@ module.run(['$templateCache', function($templateCache) {
     '				</div>\n' +
     '			</div>\n' +
     '			<div flex="50" class="Landing-angular" layout-align="center center">\n' +
-    '				<h2 class="md-display-1 Landing-laravelAngular-title">Angular</h2>\n' +
+    '				<h2 class="md-display-2 Landing-laravelAngular-title">Angular</h2>\n' +
     '				<div class="md-body-1 Landing-laravelAngular-subtitle">{{vm.angular_description}}</div>\n' +
     '				<br/>\n' +
     '				<div class="DemoCode">\n' +
@@ -105,11 +105,38 @@ module.run(['$templateCache', function($templateCache) {
     '\n' +
     '	<div class="Landing-features" layout-align="center center">\n' +
     '		<h1 class="md-display-1">Laravel Angular Material Starter</h1>\n' +
-    '		<div class="md-body-1">\n' +
+    '		<div class="md-body-1 Landing-featuresDescription">\n' +
     '			Laravel Angular Material Starter provides you with many features required when working with Laravel & Angular. Such as Json Web Token authentication, Angular generators and many more..\n' +
     '		</div>\n' +
     '		<br/>\n' +
-    '		<div>List of features coming soon..</div>\n' +
+    '		<div>\n' +
+    '			<div flex="80" flex-offset="10">\n' +
+    '				<div layout="row" layout-align="space-between">\n' +
+    '					<div flex="33">\n' +
+    '						<div class="Landing-featuresEntry Landing-featuresEntry--restful">\n' +
+    '							<md-icon md-svg-src="/img/icons/restful-api.svg" class="Landing-featuresEntry-icon"></md-icon>\n' +
+    '						</div>\n' +
+    '						<div class="md-headline Landing-featuresTitle">RESTful API</div>\n' +
+    '						<div class="md-subhead Landing-featuresDescription">Build consistent REST APIs and call them fluently using JavaScript, without having to worry about validation errors.</div>\n' +
+    '					</div>\n' +
+    '					<div flex="33">\n' +
+    '						<div class="Landing-featuresEntry Landing-featuresEntry--jwt">\n' +
+    '							<md-icon md-svg-src="/img/icons/json-webtoken.svg" class="Landing-featuresEntry-icon"></md-icon>\n' +
+    '						</div>\n' +
+    '						<div class="md-headline Landing-featuresTitle">Json Web Token Authentication</div>\n' +
+    '						<div class="md-subhead Landing-featuresDescription">Get an out-of-the-box JWT Authentication in your app.</div>\n' +
+    '					</div>\n' +
+    '					<div flex="33">\n' +
+    '						<div class="Landing-featuresEntry Landing-featuresEntry--generators">\n' +
+    '							<md-icon md-svg-src="/img/icons/angular-generators.svg" class="Landing-featuresEntry-icon"></md-icon>\n' +
+    '						</div>\n' +
+    '						<div class="md-headline Landing-featuresTitle">Angular Generators</div>\n' +
+    '						<div class="md-subhead Landing-featuresDescription">Generate angular features, dialogs, directives, services, filters & configs just like you\'re used to.</div>\n' +
+    '					</div>\n' +
+    '\n' +
+    '				</div>\n' +
+    '			</div>\n' +
+    '		</div>\n' +
     '		<br/>\n' +
     '		<br/>\n' +
     '	</div>\n' +
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <link rel="stylesheet" href="{!! asset('css/vendor.css') !!}">
     <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
-    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
     <title>Laravel 5 angular material starter</title>
     <!--[if lte IE 10]>
     <script type="text/javascript">document.location.href = '/unsupported-browser'</script>
