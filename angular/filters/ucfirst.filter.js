7a24510c22425d952968a1c09cc8e28b7a65821d
Jad Salhani <salhanijad@gmail.com>
2016-01-23 15:55:17 +0200

    Refactored filters to fit ES6 syntax

--- angular/filters/ucfirst.filter.js
+++ angular/filters/ucfirst.filter.js
@@ -1,13 +1,8 @@
-(function(){
-	"use strict";
-
-	angular.module('app.filters').filter('ucfirst', function() {
-		return function( input ) {
-			if ( !input ){
-				return null;
-			}
-			return input.substring(0, 1).toUpperCase() + input.substring(1);
-		};
-	});
-
-})();
+export function UcFirstFilter() {
+	return function(input) {
+		if (!input) {
+			return null;
+		}
+		return input.substring(0, 1).toUpperCase() + input.substring(1);
+	};
+}
