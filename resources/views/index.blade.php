19aafcd9a934fac428e4b1710d7b3756afe6e713
Jad Joubran <joubran.jad@gmail.com>
2016-02-22 22:19:58 +0100

    Add common meta tags in head. Closes #175

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,16 @@
 <!doctype html>
 <html ng-app="app" ng-strict-di>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" href="{!! asset('css/vendor.css') !!}">
     <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
     <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+
     <title>Laravel Angular Material Starter</title>
+
     <!--[if lte IE 10]>
     <script type="text/javascript">document.location.href = '/unsupported-browser'</script>
     <![endif]-->
