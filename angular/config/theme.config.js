6af82ef977daf0ba204851b3f812897ccfe4ac94
Jad Joubran <joubran.jad@gmail.com>
2016-03-25 19:26:53 +0200

    Design fixes by @nicolesaidy #202

--- angular/config/theme.config.js
+++ angular/config/theme.config.js
@@ -2,7 +2,9 @@ export function ThemeConfig($mdThemingProvider) {
 	'ngInject';
 	/* For more info, visit https://material.angularjs.org/#/Theming/01_introduction */
 	$mdThemingProvider.theme('default')
-		.primaryPalette('indigo')
+		.primaryPalette('light-blue', {
+            default: '600'
+        })
 		.accentPalette('grey')
 		.warnPalette('red');
 }
