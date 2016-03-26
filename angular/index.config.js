cfd79aed449564e601b2e381da3bf819ce655eed
Jad Joubran <joubran.jad@gmail.com>
2016-03-26 19:46:58 +0200

    removed index.routes.js and fixed path in index.config.js closes #222

--- angular/index.config.js
+++ angular/index.config.js
@@ -1,8 +1,10 @@
+import {RoutesConfig} from './config/routes.config';
 import {LoadingBar}	from './config/loading_bar.config';
 import {ThemeConfig} from './config/theme.config';
 import {SatellizerConfig} from './config/satellizer.config';
 
 angular.module('app.config')
+    .config(RoutesConfig)
 	.config(LoadingBar)
 	.config(ThemeConfig)
 	.config(SatellizerConfig);
