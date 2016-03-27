4dc33598aa233e43de69b0de49492dcb2067d0a8
Jad Joubran <joubran.jad@gmail.com>
2016-03-27 14:29:06 +0300

    auto generate tests for components & services. closes #221

--- config/generators.php
+++ config/generators.php
@@ -14,9 +14,21 @@ return [
             'component'       => '.component.js',
             'componentView'   => '.component.html',
             'dialog'          => '.dialog.js',
+            'dialogView'      => '.dialog.html',
             'service'         => '.service.js',
             'config'          => '.config.js',
             'filter'          => '.filter.js',
             'pageView'        => '.page.html',
+      ],
+      'tests' => [
+            'enable' => [
+                'components'      => true,
+                'services'        => true,
+            ],
+            'source' => [
+                'root'            => 'tests/angular/',
+                'components'      => 'app/components',
+                'services'        => 'services',
+            ],
       ],
 ];
