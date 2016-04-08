cba67a87aeb79c4ea311962f812654cfbd8735ae
flick36 <flick36@gmail.com>
2016-04-08 18:41:32 -0500

    Update routes.run.js
    Fixed a typo on the comment it now redirects to the login page, not the lading one

--- angular/run/routes.run.js
+++ angular/run/routes.run.js
@@ -5,7 +5,7 @@ export function RoutesRun($rootScope, $state, $auth) {
     var deregisterationCallback =  $rootScope.$on("$stateChangeStart", function(event, toState) {
 
         if (toState.data && toState.data.auth) {
-            /*Cancel going to the authenticated state and go back to landing*/
+            /*Cancel going to the authenticated state and go back to the login page*/
             if (!$auth.isAuthenticated()) {
                 event.preventDefault();
                 return $state.go('app.login');
