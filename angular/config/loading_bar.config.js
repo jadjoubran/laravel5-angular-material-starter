3d14ca087970753fe5ab19ba4cee1312b0db468c
Jad Joubran <joubran.jad@gmail.com>
2016-04-09 11:31:54 +0300

    consistent naming for loading bar config

--- angular/config/loading_bar.config.js
+++ angular/config/loading_bar.config.js
@@ -1,4 +1,4 @@
-export function LoadingBar(cfpLoadingBarProvider) {
+export function LoadingBarConfig(cfpLoadingBarProvider) {
 	'ngInject';
 	cfpLoadingBarProvider.includeSpinner = false;
 }
--- angular/index.config.js
+++ angular/index.config.js
@@ -1,10 +1,10 @@
 import {RoutesConfig} from './config/routes.config';
-import {LoadingBar}	from './config/loading_bar.config';
+import {LoadingBarConfig} from './config/loading_bar.config';
 import {ThemeConfig} from './config/theme.config';
 import {SatellizerConfig} from './config/satellizer.config';
 
 angular.module('app.config')
     .config(RoutesConfig)
-	.config(LoadingBar)
+	.config(LoadingBarConfig)
 	.config(ThemeConfig)
 	.config(SatellizerConfig);
