58ea49afb640a61e74e17e81e298e446949913d2
Allan Alvarez Ortiz <flick36@gmail.com>
2016-05-13 16:00:44 -0500

    Update routes.run.js

--- angular/run/routes.run.js
+++ angular/run/routes.run.js
@@ -5,12 +5,12 @@ export function RoutesRun($state, $transitions) {
         to: ($state) => $state.data && $state.data.auth
     };
 
-    let redirectToLogin = ['$auth', '$transition$', ($auth) => {
-        /*Cancel going to the authenticated state and go back to the login page*/
+    let redirectToLogin = ($auth) => {
+        'ngInject';
         if (!$auth.isAuthenticated()) {
             return $state.target('app.login', undefined, {location: false});
         }
-    }];
+    };
 
     $transitions.onBefore(requiresAuthCriteria, redirectToLogin, {priority:10});
 
