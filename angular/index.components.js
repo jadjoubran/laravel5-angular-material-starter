7048c49ae6b55899f63df9c6d47442c1abc66c33
Jad Joubran <joubran.jad@gmail.com>
2016-06-20 10:53:50 +0300

    critical rendering working but issue with font

--- angular/index.components.js
+++ angular/index.components.js
@@ -1,9 +1,11 @@
+import {AppShellComponent} from './app/components/app-shell/app-shell.component';
 import {ResetPasswordComponent} from './app/components/reset-password/reset-password.component';
 import {ForgotPasswordComponent} from './app/components/forgot-password/forgot-password.component';
 import {LoginFormComponent} from './app/components/login-form/login-form.component';
 import {RegisterFormComponent} from './app/components/register-form/register-form.component';
 
 angular.module('app.components')
+	.component('appShell', AppShellComponent)
 	.component('resetPassword', ResetPasswordComponent)
 	.component('forgotPassword', ForgotPasswordComponent)
 	.component('loginForm', LoginFormComponent)
---
+++ tests/angular/app/components/app-shell.component.spec.js
@@ -0,0 +1,11 @@
+ngDescribe({
+    name: 'Test app-shell component',
+    modules: 'app',
+    element: '<app-shell></app-shell>',
+    tests: function (deps) {
+
+        it('basic test', () => {
+            //
+        });
+    }
+});
