91cd3e25d20c94826bcde5f3a2c07aa1f42745b9
Jad Joubran <joubran.jad@gmail.com>
2016-07-03 23:55:14 +0300

    no need to reload gulp to get the newest precache-config.json

--- tasks/swPrecache.task.js
+++ tasks/swPrecache.task.js
@@ -3,13 +3,18 @@ var swPrecache = require('sw-precache');
 
 var Elixir = require('laravel-elixir');
 
-var preCacheConfig = require('./../precache-config.json');
-
 var Task = Elixir.Task;
 
 Elixir.extend('swPrecache', function() {
 
     new Task('generateServiceWorker', function() {
+
+        /*
+          always read the latest config. Allows changing
+          precache-config.json without reloading gulp
+        */
+        delete require.cache[require.resolve('./../precache-config.json')]
+        var preCacheConfig = require('./../precache-config.json');
 
         return swPrecache.write(path.join('public', 'service-worker.js'), preCacheConfig);
 
