cf28bcb69c9295c2fdd7fc526be67eb6e663f57d
Chihab Otmani <chihab@gmail.com>
2016-07-10 14:54:29 +0100

    Fixes issues #356 and #358 (#361)
    * fixed package.json issue #353
    
    * test travis build
    
    * fixed package.json issue #353
    
    * add composer install and move install commands to install step
    
    * move php artisan migrate to script step
    
    * move php artisan key and jwg generation to composer post installation phase
    
    * replace create-project phases by install ones
    
    * fix previous commit
    
    * [fix] issue #358: sassify critical inlined style for pwa and add gulp task to handle it

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,8 @@
     <script type="text/javascript">document.location.href = '/unsupported-browser'</script>
     <![endif]-->
 
-    <style><?php require("../angular/critical.css") ?></style>
+    <style><?php require( public_path( "css/critical.css" ) ) ?></style>
+    
 </head>
 <body>
 
