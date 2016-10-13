7839beb2728140ab8bc6ca1e901b68e01635c68b
Yazid HANIFI <hanifi.yazid@gmail.com>
2016-10-13 13:28:57 +0200

    Fixed gulpfile bug (#417)
    * Fixed #414: Removed ./ prefix from paths.
    
    This issue is related to Elixir issue https://github.com/laravel/elixir/issues/623.
    
    * Fixed #414: Set Elixir assets paths config

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,12 @@ require('laravel-elixir-eslint');
 require('./tasks/swPrecache.task.js');
 require('./tasks/bower.task.js');
 
+// setting assets paths
+elixir.config.assetsPath = './';
+elixir.config.css.folder = 'angular';
+elixir.config.css.sass.folder = 'angular';
+elixir.config.js.folder = 'angular';
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -23,9 +29,11 @@ require('./tasks/bower.task.js');
          'public/css/final.css'
      ],
      scripts = [
-         './public/js/vendor.js', './public/js/app.js'
+         'public/js/vendor.js', 'public/js/app.js'
      ],
      styles = [
+         // for some reason, ./ prefix here works fine!
+         // it is needed to override elixir.config.css.folder for styles mixin
          './public/css/vendor.css', './public/css/app.css'
      ],
      karmaJsDir = [
@@ -38,12 +46,12 @@ require('./tasks/bower.task.js');
 
 elixir(mix => {
     mix.bower()
-       .copy('./angular/app/**/*.html', './public/views/app/')
-       .webpack('./angular/index.main.js', './public/js/app.js')
-       .sass(['./angular/**/*.scss', '!./angular/critical.scss'], 'public/css')
-       .sass('./angular/critical.scss', 'public/css/critical.css')
-       .styles(styles, './public/css/final.css')
-       .eslint('./angular/**/*.js')
+       .copy('angular/app/**/*.html', 'public/views/app/')
+       .webpack('index.main.js', 'public/js/app.js')
+       .sass(['**/*.scss', 'critical.scss'], 'public/css')
+       .sass('critical.scss', 'public/css/critical.css')
+       .styles(styles, 'public/css/final.css')
+       .eslint('angular/**/*.js')
        .combine(scripts, 'public/js/final.js')
        .version(assets)
        .swPrecache();
